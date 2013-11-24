d1bc34b6d1ac1e26a02a6026316567e3268c0b65
承风 <libiao.lb@taobao.com>
2013-11-24 18:03:39 +0800

    add a simple demo page

---
+++ demo/index.js
@@ -0,0 +1,65 @@
+(function() {
+    var floater = $('.floater');
+    var tl = new TimelineLite();
+    tl.add(TweenLite.to(floater, 2.5, {
+        left : '300px',
+        top : '0px',
+        width: '30px',
+        height: '30px',
+        ease : Linear.easeNone
+    }));
+    tl.add(TweenLite.to(floater, 2.5, {
+        left : '300px',
+        top : '200px',
+        width: '40px',
+        height: '40px',
+        ease : Linear.easeNone
+    }));
+    tl.add(TweenLite.to(floater, 2.5, {
+        left : '0px',
+        top : '200px',
+        width: '50px',
+        height: '50px',
+        ease : Linear.easeNone
+    }));
+    tl.add(TweenLite.to(floater, 2.5, {
+        left : '0px',
+        top : '0px',
+        width: '20px',
+        height: '20px',
+        ease : Linear.easeNone,
+        onComplete : animationWithJs
+    }));
+
+    //jQuery animation
+    function animationWithJs() {
+        floater.animate({
+            left : '300px',
+            top : '0px',
+            width: '30px',
+            height: '30px'
+        }, 2500, 'linear', function() {
+            floater.animate({
+                left : '300px',
+                top : '200px',
+                width: '40px',
+                height: '40px'
+            }, 2500, 'linear', function() {
+                floater.animate({
+                    left : '0px',
+                    top : '200px',
+                    width: '50px',
+                    height: '50px'
+                }, 2500, 'linear', function() {
+                    floater.animate({
+                        left : '0px',
+                        top : '0px',
+                        width: '20px',
+                        height: '20px'
+                    }, 2500, 'linear');
+                });
+            });
+        });
+    }
+
+})();
